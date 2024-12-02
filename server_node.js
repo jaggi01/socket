8c3166088e788dbc5e82ce50387acbeb14be778c
jaggi01 <jagjeetagarwal@gmail.com>
2024-12-02 14:10:39 +0530

    id added

--- server_node.js
+++ server_node.js
@@ -1,55 +1,45 @@
 const net = require('net');
 
-// Create a TCP server
+// Create a TCP server to handle socket1 and socket2 connections
 const server = net.createServer();
 
 let socket1 = null; // RS-232 connected socket
 let socket2 = null; // DLMS COSEM connected socket
 
 server.on('connection', (socket) => {
+  // Assume the first connection is socket1 (RS-232)
   if (!socket1) {
     socket1 = socket;
     console.log('Socket1 (RS-232) connected');
-    
+    // socket1.write('sksjkfsajkjflksdjflkjsd');
+    // Listen for data from socket1 and relay it to socket2
     socket1.on('data', (data) => {
-      // Convert data to string if it's binary
-      const dataStr = data.toString('utf-8');
-
-      // Split the data into identity package and buffer
-      const [identityPackage, ...bufferParts] = dataStr.split('_');
-
-      // Check if we successfully split the data
-      if (identityPackage && socket2) {
-        console.log("Identity Package from socket1 (RS-232):", identityPackage);
-        socket2.write(identityPackage); // Send identity package to socket2
+      // if (socket1) {
+      //   console.log("Data receive1d from socket1 (RS-232) Copy:", data.toString('utf-8'));
+      // }
+      if (socket2) {
+        console.log("Data receive1d from socket1 (RS-232):", data.toString('utf-8'));
+        socket2.write(data); // Send data to socket2
       }
-
-      // Join the remaining parts of the buffer and send to socket2
-      const bufferData = bufferParts.join('_');
-      if (bufferData && socket2) {
-        console.log("Buffer data from socket1 (RS-232):", bufferData);
-        socket2.write(bufferData); // Send buffer data to socket2
-      }
+      // console.log("Data receive1d from socket1 (RS-232):", data.toString('utf-8'));
     });
 
     socket1.on('end', () => {
       console.log('Socket1 (RS-232) disconnected');
       socket1 = null;
     });
-
-    socket1.on('error', (err) => {
-      console.log('Socket1 error:', err.message);
-      socket1 = null;
-    });
-
+    
+  // Assume the second connection is socket2 (DLMS COSEM)
+  // }
   } else if (!socket2) {
     socket2 = socket;
     console.log('Socket2 (DLMS COSEM) connected');
     
+    // Listen for data from socket2 and relay it to socket1
     socket2.on('data', (data) => {
       if (socket1) {
         console.log("Data received from socket2 (DLMS COSEM):", data);
-        socket1.write(data); // Relay to socket1
+        socket1.write(data); // Send data to socket1
       }
     });
 
@@ -57,15 +47,10 @@ server.on('connection', (socket) => {
       console.log('Socket2 (DLMS COSEM) disconnected');
       socket2 = null;
     });
-
-    socket2.on('error', (err) => {
-      console.log('Socket2 error:', err.message);
-      socket2 = null;
-    });
   }
 });
 
-// Start the server and listen on port 3000
+// Start the server
 server.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
